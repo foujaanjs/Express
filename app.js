d14afdd9e51e5ceec8ca87961ebff2ab77ee1171
foujaanjs <129575221+foujaanjs@users.noreply.github.com>
2025-03-05 15:56:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
+const bodyparser = require("body-parser");
+const path = require("path");
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Foujaan');
+const port = 4000;
+ 
+app.use(bodyparser.json());
+
+let users =[
+    {id: 1,name: 'oviyan', email: 'oviya17@gmail.com'},
+    {id: 2,name: 'bhagavath', email: 'bhagvid0225@gmail.com'},
+    {id: 3,name: 'toji', email: 'tojifusi001@gmail.com'},
+    {id: 4,name: 'oviya', email: 'oviya7@gmail.com'},
+    {id: 5,name: 'thor', email: 'asaguardian17@gmail.com'},
+    {id: 6,name: 'chottabheem', email: 'oviyanpur27@gmail.com'},
+];
+
+app.get('/users',(req,res)=> {
+    res.json(users);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/users/:id',(req,res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+
+if(user){
+    res.json(user);
+}else{
+    res.status(404).json({message : "User not Found"});
+}
+});
+
+app.post('/users',(req,res) => {
+    const newUser = {
+        id : users.length+1,
+        name : req.body.name,
+        email : req.body.email
+
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:4000');
 });
